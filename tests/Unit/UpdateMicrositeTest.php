bb786cfdd5456a238e7faf9e0f068a3f870b36f9
Gabriela Munoz Nupan <gabriela@codelar.co>
2024-07-17 22:16:52 -0500

    test: new test and add report coverage in sonar

--- tests/Unit/UpdateMicrositeTest.php
+++ tests/Unit/UpdateMicrositeTest.php
@@ -2,19 +2,31 @@
 
 use App\Domain\Categories\Models\Category;
 use App\Domain\Currencies\Models\Currency;
-use App\Domain\Microsites\Models\MicrositeType;
+use App\Domain\Microsites\Actions\UpdateMicrosite;
+use App\Domain\Microsites\Models\Microsite;
 use App\Support\Definitions\MicrositesTypes;
 use App\Support\Definitions\Status;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 
 test('update microsite', function () {
-    MicrositeType::factory()->create([
-        'id' => MicrositesTypes::SUBSCRIPTIONS->value,
-        'name' => MicrositesTypes::SUBSCRIPTIONS->name,
+    $microsite = Microsite::factory()->create();
+    $params['fields'] = [
+        'microsites_type_id' => $microsite->microsites_type_id,
+        'category_id' => $microsite->category_id,
+        'name' => 'Test microsite',
+        'logo_path' => UploadedFile::fake()
+            ->image('microsite_image.png', 640, 480),
+        'description' => 'Test update description',
+        'currency_id' => $microsite->currency_id,
         'status' => Status::ACTIVE->value,
-    ]);
+    ];
 
+    $params['id'] = $microsite->id;
+    $this->assertTrue(UpdateMicrosite::execute($params));
+});
+
+test('generate exception', function () {
     $category = Category::factory()->create([
         'name' => Str::limit(fake()->unique()->name(), 10),
         'status' => Status::ACTIVE->value,
@@ -25,14 +37,16 @@ test('update microsite', function () {
         'status' => Status::ACTIVE->value,
     ]);
 
-    $params = [
+    $params['fields'] = [
         'microsites_type_id' => MicrositesTypes::SUBSCRIPTIONS->value,
         'category_id' => $category->id,
-        'name' => 'Test microsite',
+        'name' => 'Test update microsite',
         'logo_path' => UploadedFile::fake()
             ->image('microsite_image.png', 640, 480),
-        'description' => 'Test description',
+        'description' => 'Test update description',
         'currency_id' => $currency->id,
         'status' => Status::ACTIVE->value,
     ];
+
+    $this->assertFalse(UpdateMicrosite::execute($params));
 });
