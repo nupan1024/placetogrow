030c80dea226df6e981f0eaeb7d9be72c3d43764
Gabriela Munoz Nupan <gabriela@codelar.co>
2024-09-23 20:49:20 -0500

    feat: adjust text of tooltip subscriptions

--- lang/en/subscriptions.php
+++ lang/en/subscriptions.php
@@ -8,7 +8,7 @@ return [
     'delete' => 'Delete subscription',
     'amount' => 'Amount',
     'msj_delete' => 'Are you sure to delete the subscription',
-    'tooltip' => 'Search by name, email or role',
+    'tooltip' => 'Search by description, name, currency, amount, time to expire, billing frequency or microsite',
     'success_create' => 'The subscription was created successfully.',
     'success_update' => 'The subscription was updated successfully.',
     'success_delete' => 'The subscription was deleted successfully.',
--- lang/es/subscriptions.php
+++ lang/es/subscriptions.php
@@ -8,7 +8,7 @@ return [
     'delete' => 'Eliminar suscripción',
     'amount' => 'Valor',
     'msj_delete' => '¿Estás seguro de eliminar el suscripción',
-    'tooltip' => 'Busca por nombre, correo electrónico o rol',
+    'tooltip' => 'Buscar por descripción, nombre, moneda, valor, tiempo de expiración o frecuencia de facturación',
     'success_create' => 'Se creó el suscripción con éxito.',
     'success_update' => 'Se actualizó el suscripción con éxito.',
     'success_delete' => 'Se eliminó la suscripción con éxito.',
